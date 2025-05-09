dbc0d163f65711078d053498b9d2f6c691580c7a
Your Name <rotiminicol117@gmail.com>
2025-05-08 23:58:58 -1100

    update

--- vite.config.ts
+++ vite.config.ts
@@ -3,16 +3,19 @@ import react from "@vitejs/plugin-react-swc";
 import path from "path";
 import { componentTagger } from "lovable-tagger";
 
-// https://vitejs.dev/config/
 export default defineConfig(({ mode }) => ({
   server: {
     host: "::",
     port: 8080,
   },
+  preview: {
+    host: true,
+    port: 10000, // optional, but recommended for Render
+    allowedHosts: ["mi-amour-control-panel.onrender.com"],
+  },
   plugins: [
     react(),
-    mode === 'development' &&
-    componentTagger(),
+    mode === 'development' && componentTagger(),
   ].filter(Boolean),
   resolve: {
     alias: {
