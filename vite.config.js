98be8144c138c03c52921fe6039fec8d42e537c4
Your Name <rotiminicol117@gmail.com>
2025-05-09 05:24:54 -1100

    update

--- vite.config.js
+++ vite.config.js
@@ -1,7 +1,13 @@
 import { defineConfig } from 'vite'
+import path from 'path'
 
 export default defineConfig({
   preview: {
     allowedHosts: ['miamouradminpanel.onrender.com']
+  },
+  resolve: {
+    alias: {
+      '@': path.resolve(__dirname, './src')
+    }
   }
 })
