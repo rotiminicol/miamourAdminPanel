c6f95a79e401b53f54bb6d41bf38d5bba2c5382d
Your Name <rotiminicol117@gmail.com>
2025-05-09 08:39:49 -1100

    update

--- vite.config.js
+++ vite.config.js
@@ -1,8 +1,16 @@
 import { defineConfig } from 'vite'
+import react from '@vitejs/plugin-react'
+import path from 'path'
 
 export default defineConfig({
+  plugins: [react()],
   server: {
     host: '0.0.0.0',
     port: process.env.PORT || 3000,
+  },
+  resolve: {
+    alias: {
+      '@': path.resolve(__dirname, './src'),
+    },
   },
 })
